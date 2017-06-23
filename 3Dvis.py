ad5e0720bf69b49a3d5cef8da3871f07b2b9ade3
Haylee Fay Ham <hayleeham@gmail.com>
2017-06-23 09:29:51 -0500

    restrict to 69 time periods

--- 3Dvis.py
+++ 3Dvis.py
@@ -34,15 +34,6 @@ OPTIONS =
   zMax: 4
   zMin: -1
 
-# To create custom model extensions that will render on to the HTML canvas
-# or into the DOM, we must create a View subclass for the model. Currently
-# Bokeh models and views are based on BackBone. More information about
-# using Backbone can be found here:
-#
-#     http://backbonejs.org/
-#
-# In this case we will subclass from the existing BokehJS ``LayoutDOMView``,
-# corresponding to our
 export class Surface3dView extends LayoutDOMView
 
   initialize: (options) ->
@@ -57,24 +48,12 @@ export class Surface3dView extends LayoutDOMView
     document.querySelector("head").appendChild(script)
 
   _init: () ->
-    # Create a new Graph3s using the vis.js API. This assumes the vis.js has
-    # already been loaded (e.g. in a custom app template). In the future Bokeh
-    # models will be able to specify and load external scripts automatically.
-    #
-    # Backbone Views create <div> elements by default, accessible as @el. Many
-    # Bokeh views ignore this default <div>, and instead do things like draw
-    # to the HTML canvas. In this case though, we use the <div> to attach a
-    # Graph3d to the DOM.
     @_graph = new vis.Graph3d(@el, @get_data(), OPTIONS)
 
-    # Set Backbone listener so that when the Bokeh data source has a change
-    # event, we can process the new data
     @connect(@model.data_source.change, () =>
         @_graph.setData(@get_data())
     )
 
-  # This is the callback executed when the Bokeh data has an change. Its basic
-  # function is to adapt the Bokeh data source to the vis.js DataSet format.
   get_data: () ->
     data = new vis.DataSet()
     source = @model.data_source
@@ -87,24 +66,12 @@ export class Surface3dView extends LayoutDOMView
       })
     return data
 
-# We must also create a corresponding JavaScript Backbone model sublcass to
-# correspond to the python Bokeh model subclass. In this case, since we want
-# an element that can position itself in the DOM according to a Bokeh layout,
-# we subclass from ``LayoutDOM``
 export class Surface3d extends LayoutDOM
 
-  # This is usually boilerplate. In some cases there may not be a view.
   default_view: Surface3dView
 
-  # The ``type`` class attribute should generally match exactly the name
-  # of the corresponding Python class.
   type: "Surface3d"
 
-  # The @define block adds corresponding "properties" to the JS model. These
-  # should basically line up 1-1 with the Python model class. Most property
-  # types have counterparts, e.g. ``bokeh.core.properties.String`` will be
-  # ``p.String`` in the JS implementatin. Where the JS type system is not yet
-  # as rich, you can use ``p.Any`` as a "wildcard" property type.
   @define {
     x:           [ p.String           ]
     y:           [ p.String           ]
@@ -115,39 +82,14 @@ export class Surface3d extends LayoutDOM
 """
 
 
-# This custom extension model will have a DOM view that should layout-able in
-# Bokeh layouts, use ``LayoutDOM`` as the base class. If you wanted to create
-# a custom tool, you could inherit from ``Tool``, or from ``Glyph`` if you
-# wanted to create a custom glyph, etc.
 class Surface3d(LayoutDOM):
-
-    # The special class attribute ``__implementation__`` should contain a
-    # string of JavaScript (or CoffeeScript) code that implements the
-    # JavaScript side of the custom extension model.
     __implementation__ = JS_CODE
 
-    # Below are all the "properties" for this model. Bokeh properties are
-    # class attributes that define the fields (and their types) that can be
-    # communicated automatically between Python and the browser. Properties
-    # also support type validation. More information about properties in
-    # can be found here:
-    #
-    #    http://bokeh.pydata.org/en/latest/docs/reference/core.html#bokeh-core-properties
-
-    # This is a Bokeh ColumnDataSource that can be updated in the Bokeh
-    # server by Python code
     data_source = Instance(ColumnDataSource)
 
-    # The vis.js library that we are wrapping expects data for x, y, z, and
-    # color. The data will actually be stored in the ColumnDataSource, but
-    # these properties let us specify the *name* of the column that should
-    # be used for each field.
     x = String
-
     y = String
-
     z = String
-
     color = String
 
 
@@ -176,9 +118,9 @@ bpath_source = ColumnDataSource(data=dict(bpath=bpath))
 surface = Surface3d(x="x", y="y", z="z", color="color", data_source=source)
 
 # Line graph for Kpath
-kpath = tpi_vars['Kpath']
-time = range(89)
-circle_color = ['blue'] + ['white']*88
+kpath = tpi_vars['Kpath'][:69]
+time = range(69)
+circle_color = ['blue'] + ['white']*68
 kplot_source = ColumnDataSource(data=dict(x=time, y=kpath,
                                 circle_color=circle_color))
 
@@ -208,7 +150,7 @@ callback = CustomJS(args=dict(source=source, bpath_source=bpath_source,
             z[i] = b[i];
     }
 
-    kdata['circle_color'] = Array(89).fill('white');
+    kdata['circle_color'] = Array(69).fill('white');
     kdata['circle_color'][time] = 'blue';
 
     source.change.emit();
@@ -216,7 +158,7 @@ callback = CustomJS(args=dict(source=source, bpath_source=bpath_source,
 """)
 
 # time slider
-time_slider = Slider(start=0, end=88, value=0, step=1, title='Time period',
+time_slider = Slider(start=0, end=68, value=0, step=1, title='Time period',
                      callback=callback)
 callback.args['time'] = time_slider
 
